3b721819f4f3a59346cf46ea4f6ad747d0cb8ad7
SUMAN SINGHA <sumansingha047@gmail.com>
2024-11-01 23:59:59 +0530

    Basic short video system is almost complete

--- lib/pages_list.dart
+++ lib/pages_list.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone_fcc_2024/features/content/Long_video/widgets/long_video_screen.dart';
+import 'package:youtube_clone_fcc_2024/features/content/short_video/pages/short_video_page.dart';
+import 'package:youtube_clone_fcc_2024/features/search/pages/search_screen.dart';
 
 const pages = [
+  SearchScreen(),
   LongVideoScreen(),
-  Center(
-    child: Text("shorts"),
-  ),
+  ShortVideoPage(),
   Center(
     child: Text("upload"),
   ),
   Center(
-    child: Text("Home"),
+    child: Text("upload"),
   ),
   Center(
     child: Text("Home"),
